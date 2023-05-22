46acb3008566754ab273549b3dbfde0ae6b9b68b
sam-laptop-vm <seadas-user@localhost>
2023-05-22 19:48:09 +0100

    Getting the SAM model running #1

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
-# ogc_testbed19_mlmodels
+# OGC Testbed-19 Machine Learning models contribution
 
-Testbed 19 Machine Learning models task
+This repository contains content related to the Testbed 19 Machine Learning models task
 
-Tests being performed:
+The tests being performed by Pixalytics Ltd included:
 * Installing and testing Meta SAM model with RGB and hyperspectral data (this repo)
-* Testing plastics model for transfer learning (Marlisat repo)
+* Testing plastics model for transfer learning (seperate repo)
 
 # Installing environment and setting up SAM model
 
@@ -13,3 +13,4 @@ Tests being performed:
 * Install SAM model: `pip install git+https://github.com/facebookresearch/segment-anything.git`
 
 * Download SAM weights: `wget -q https://dl.fbaipublicfiles.com/segment_anything/sam_vit_h_4b8939.pth`
+
--- test_meta_SAM_rgb.py
+++ test_meta_SAM_rgb.py
@@ -1,39 +1,35 @@
-
 import os
-import cv2
 import numpy as np
+import cv2
+import supervision as sv
 import torch
 from segment_anything import sam_model_registry, SamAutomaticMaskGenerator
 
 home = os.path.expanduser("~")
 IMAGE_PATH = os.path.join(home, "shared/testbed19")
-CHECKPOINT_PATH = os.path.join(IMAGE_PATH,"checkpoint")
+CHECKPOINT_PATH = os.path.join(IMAGE_PATH, "checkpoint")
 
-def save_png(image, outfile):
+
+def save_png(image_bgr, result, outfile):
+    # Annotate original image
+    mask_annotator = sv.MaskAnnotator()
+    print("Result: ", result)
+    detections = sv.Detections.from_sam(result)
+    image = mask_annotator.annotate(image_bgr, detections)
 
     # Convert to bytes
     image = image.astype(np.uint8)
+
     # Write
-    cv2.imwrite(outfile,image)
+    cv2.imwrite(outfile, image)
 
 
-def run_SAM():
-
+def run_meta_sam(image_rgb):
     DEVICE = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
     MODEL_TYPE = "vit_h"
 
-    sam = sam_model_registry[MODEL_TYPE]()#checkpoint=CHECKPOINT_PATH)
+    sam = sam_model_registry[MODEL_TYPE]()  # checkpoint=CHECKPOINT_PATH)
     sam.to(device=DEVICE)
-
-    # Input and output file
-    infile = os.path.join(IMAGE_PATH,'input_rgb.png')
-    if not os.path.exists(infile):
-        print("Failed to access {}".format(infile))
-        return None
-
-    # Read input file
-    image_bgr = cv2.imread(infile)
-    image_rgb = cv2.cvtColor(image_bgr, cv2.COLOR_BGR2RGB)
 
     # Generate mask
     mask_generator = SamAutomaticMaskGenerator(sam)
@@ -42,19 +38,28 @@ def run_SAM():
     return result
 
 
-
 def main():
+    # Check input image exists
+    infile = os.path.join(IMAGE_PATH, 'input_rgb.png')
+    if not os.path.exists(infile):
+        print("Failed to access {}".format(infile))
+        return None
+
+    # Read input file
+    image_bgr = cv2.imread(infile)
+    image_rgb = cv2.cvtColor(image_bgr, cv2.COLOR_BGR2RGB)
 
     # Run the SAM model
-    result = run_SAM()
+    result = run_meta_sam(image_rgb)
 
     # Save to png file
-    if result is not None:
-        outfile = os.path.join(IMAGE_PATH,'output_rgb.png')
+    if len(result) > 0:
+        outfile = os.path.join(IMAGE_PATH, 'output_rgb.png')
         if os.path.exists(outfile):
             os.remove(outfile)
-        save_png(result, outfile)
-
+        save_png(image_bgr, result, outfile)
+    else:
+        print("No result returned: {}".format(result))
 
 
 if __name__ == "__main__":
